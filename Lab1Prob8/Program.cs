9330d6807b2214547a05db888d96ddd34d0b64e5
Ada Ababei <contact@adina_ababei@yahoo.com>
2023-09-19 22:53:07 +0300

    Prob8

--- Lab1Prob8/Program.cs
+++ Lab1Prob8/Program.cs
@@ -8,6 +8,41 @@ namespace Lab1Prob8
     {
         static void Main(string[] args)
         {
+            /*
+             * Ex 8
+Scrieti un program care interschimba valoarea a doua variabile intregi.
+            */
+            int x = 2;
+            int y = 6;
+
+            switch (x)
+            {
+
+                case 1:
+                    Console.WriteLine("Numer 1 was found");
+                    break;
+                case 2:
+                    Console.WriteLine("Numer 2 was found");
+                    break;
+                case 3:
+                    Console.WriteLine("Numer 3 was found");
+                    break;
+                case 4:
+                    Console.WriteLine("Numer 4 was found");
+                    break;
+                case 5:
+                    Console.WriteLine("Numer 5 was found");
+                    break;
+                case 6:
+                    Console.WriteLine("Numer 6 was found");
+                    break;
+                case 7:
+                    Console.WriteLine("Numer 7 was found");
+                    break;
+                default:
+                    Console.WriteLine("Number nor found");
+                    break;
+            }
         }
     }
 }
